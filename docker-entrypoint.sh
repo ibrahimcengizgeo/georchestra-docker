decf942391944a66e6d016440ab5f278569f8671
ibrahimcengizgeo <106270940+ibrahimcengizgeo@users.noreply.github.com>
2022-06-20 10:32:39 +0300

    added entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt-get update
+apt-get install libgdal-java
+ln -s /usr/lib/jni/libgdalalljni.so.20 /usr/lib/libgdalalljni.so
+
+
+DIR=/docker-entrypoint.d
+
+if [[ -d "$DIR" ]]
+then
+  /bin/run-parts --verbose "$DIR"
+fi
+
+exec "$@"
